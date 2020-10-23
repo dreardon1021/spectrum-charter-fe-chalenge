c30e6609d8a2b752323b16ccfe641f5f235ca03a
Dan Reardon <dreardon1021@gmail.com>
2020-10-23 17:21:45 -0600

    Complete fetch call and add it to state on render

--- src/App/App.js
+++ src/App/App.js
@@ -10,7 +10,14 @@ import './App.css';
    }
 
    componentDidMount() {
-
+     fetch('https://code-challenge.spectrumtoolbox.com/api/restaurants', {
+      method: 'GET',
+      headers: {
+        'Authorization': 'Api-Key q3MNxtfep8Gt'
+      }
+     })
+     .then(response => response.json())
+     .then(data => this.setState({restaurantData: data}))
    }
 
   render() {
