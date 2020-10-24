76aa31a13497ca0f2dc10aec531651fc7fa1e2ab
Dan Reardon <dreardon1021@gmail.com>
2020-10-23 19:28:23 -0600

    Have search and allData in state, setup reset logo

--- src/App/App.js
+++ src/App/App.js
@@ -1,29 +1,37 @@
-import React, { Component } from 'react'
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
- class App extends Component {
-   constructor() {
-     super();
-     this.state = {
-       restaurantData: []
-     }
-   }
+import Nav from "../Nav/Nav";
 
-   componentDidMount() {
-     fetch('https://code-challenge.spectrumtoolbox.com/api/restaurants', {
-      method: 'GET',
+class App extends Component {
+  constructor() {
+    super();
+    this.logoResetTable = this.logoResetTable.bind(this)
+    this.state = {
+      allRestaurantData: [],
+      currentSearchData: []
+    };
+  }
+
+  logoResetTable(allData) {
+    this.setState({ currentSearchData: allData });
+  }
+
+  componentDidMount() {
+    fetch("https://code-challenge.spectrumtoolbox.com/api/restaurants", {
+      method: "GET",
       headers: {
-        'Authorization': 'Api-Key q3MNxtfep8Gt'
+        Authorization: "Api-Key q3MNxtfep8Gt"
       }
-     })
-     .then(response => response.json())
-     .then(data => this.setState({restaurantData: data}))
-   }
+    })
+      .then(response => response.json())
+      .then(data => this.setState({ allRestaurantData: data, currentSearchData: data }));
+  }
 
   render() {
     return (
       <main>
-        
+        <Nav logoResetTable={this.logoResetTable} allRestaurantData={this.allRestaurantData} />
       </main>
     );
   }
