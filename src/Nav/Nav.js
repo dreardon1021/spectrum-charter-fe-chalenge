94af21fa5fb8e9b399ab14ae73dcda818f94c43d
Dan Reardon <dreardon1021@gmail.com>
2020-10-23 19:29:18 -0600

    Add styles to nav and setup nav

---
+++ src/Nav/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import './Nav.css'
+import '../App/App.css'
+
+
+
+const Nav = ({ logoResetTable, allRestaurantData }) => {
+  return(
+    <nav>
+      <h1 onClick={allRestaurantData => logoResetTable(allRestaurantData)} className="logo cursor-pointer">Restauraunt Locator</h1>
+    </nav>
+  )
+}
+
+export default Nav
