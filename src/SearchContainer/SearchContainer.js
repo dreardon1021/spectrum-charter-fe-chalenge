389ef18e70f6ab0c9d2317b76b68c0ef478af0c4
Dan Reardon <dreardon1021@gmail.com>
2020-10-24 14:13:15 -0600

    Pass down possible state/genres via props

--- src/SearchContainer/SearchContainer.js
+++ src/SearchContainer/SearchContainer.js
@@ -2,11 +2,16 @@ import React from "react";
 import "./SearchContainer.css";
 
 import SearchForm from "../SearchForm/SearchForm";
+import FilterButtonContainer from "../FilterButtonContainer/FilterButtonContainer";
 
-const SearchContainer = ({ updateCurrentSearchData }) => {
+const SearchContainer = ({ updateCurrentSearchData, possibleRestaurantGenres, possibleRestaurantStates }) => {
   return (
     <section className="search-container">
       <SearchForm updateCurrentSearchData={updateCurrentSearchData} />
+      <FilterButtonContainer
+        possibleRestaurantStates={possibleRestaurantStates}
+        possibleRestaurantGenres={possibleRestaurantGenres}
+      />
     </section>
   );
 };
