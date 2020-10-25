2674a928ef300c4f0d6cd84f6530bc51a36e5f9d
Dan Reardon <dreardon1021@gmail.com>
2020-10-25 14:44:05 -0600

    Add functional paginated restaurant table

---
+++ src/RestaurantTable/RestaurantTable.js
@@ -0,0 +1,88 @@
+import React, { Component } from "react";
+import "./RestaurantTable.css";
+
+class RestaurantTable extends Component {
+  constructor({ dataToDisplay }) {
+    super({ dataToDisplay });
+    this.handleClick = this.handleClick.bind(this);
+    this.state = {
+      currentPage: 1,
+      restaurantsPerPage: 10
+    };
+  }
+
+  handleClick(e) {
+    this.setState({
+      currentPage: Number(e.target.id)
+    });
+  }
+
+  render() {
+    const { currentPage, restaurantsPerPage } = this.state;
+
+    const indexOfLastRestaurant = currentPage * restaurantsPerPage;
+    const indexOfFirstRestaurant = indexOfLastRestaurant - restaurantsPerPage;
+    const currentRestaurants = this.props.dataToDisplay.slice(
+      indexOfFirstRestaurant,
+      indexOfLastRestaurant
+    );
+
+    const renderRestaurants = currentRestaurants.map((restaurant, index) => {
+      let restaurantGenres = restaurant.genre.split(",").join(" ");
+      return (
+        <tr className="restaurant-table-row" key={index}>
+          <td className="restaurant-table-data-cell name-cell">
+            {restaurant.name}
+          </td>
+          <td className="restaurant-table-data-cell city-cell">
+            {restaurant.city}
+          </td>
+          <td className="restaurant-table-data-cell state-cell">
+            {restaurant.state}
+          </td>
+          <td className="restaurant-table-data-cell telephone-cell">
+            {restaurant.telephone}
+          </td>
+          <td className="restaurant-table-data-cell genre-cell">
+            {restaurantGenres}
+          </td>
+        </tr>
+      );
+    });
+
+    const pageNumbers = [];
+    for (
+      let i = 1;
+      i <= Math.ceil(this.props.dataToDisplay.length / restaurantsPerPage);
+      i++
+    ) {
+      pageNumbers.push(i);
+    }
+
+    const renderPageNumbers = pageNumbers.map(number => {
+      return (
+        <li key={number}>
+          <button
+            id={number}
+            onClick={this.handleClick}
+            className="page-button cursor-pointer"
+          >
+            {number}
+          </button>
+        </li>
+      );
+    });
+
+    return (
+      <section className="table-container">
+        <h2 className="table-header">Available Restaurants</h2>
+        <table calssName="restaurant-table">
+          <tbody classname="restaurant-table-body">{renderRestaurants}</tbody>
+        </table>
+        <ul className="page-numbers-container">{renderPageNumbers}</ul>
+      </section>
+    );
+  }
+}
+
+export default RestaurantTable;
