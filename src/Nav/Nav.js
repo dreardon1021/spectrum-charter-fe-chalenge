9e07eea87f792826ed4847d96b5453210496b348
Dan Reardon <dreardon1021@gmail.com>
2020-10-24 18:00:38 -0600

    Fix onClick function to refresh data

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -8,7 +8,7 @@ import '../App/App.css'
 const Nav = ({ logoResetTable, allRestaurantData }) => {
   return(
     <nav>
-      <h1 onClick={allRestaurantData => logoResetTable(allRestaurantData)} className="logo cursor-pointer">Restauraunt Locator</h1>
+      <h1 onClick={() => logoResetTable(allRestaurantData)} className="logo cursor-pointer">Restauraunt Locator</h1>
     </nav>
   )
 }
